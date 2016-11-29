f1395d1c19378b4db119de112329121ce23029bb
ywsuen <yw_suen@outlook.com>
2016-11-29 22:02:43 +0800

    App.scala改动

--- src/main/scala/test/App.scala
+++ src/main/scala/test/App.scala
@@ -1,10 +1,14 @@
 package test
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
  * Hello
  **/
 object App {
   def main(args: Array[String]) = {
+    val sc = new SparkContext(new SparkConf().setAppName("test").setMaster("local[*]"))
     println("Hello")
+    sc.stop()
   }
 }
